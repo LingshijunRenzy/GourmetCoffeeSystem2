ce42f4fac4d101413ef5ea51bbf31ad56c4c6bf6
LingshijunRenzy <598364536@qq.com>
2024-04-30 17:16:34 +0800

    update HtmlFormatters

--- src/framework/FancyHtmlFormatter.java
+++ src/framework/FancyHtmlFormatter.java
@@ -1,18 +1,29 @@
 package framework;
 
 public class FancyHtmlFormatter extends HtmlFormatter{
+    private boolean responsive;
     private String animation;
-
 
 
     public FancyHtmlFormatter(){
         super();
-        this.animation = "animation: rotate 2s linear infinite;";
+        this.responsive = true;
+        this.animation = "mymove 5s infinite";
     }
 
 
-
+    @Override
     public String formatSales(Sales sales){
-        return super.formatSales(sales);
+        String html = super.formatSales(sales);
+        if(responsive){
+            String css = "<style>@media screen and (max-width: 600px) {body { font-size: 18px; }}</style>";
+            html = html.replace("</style>", css + "</style>");
+        }
+        if(!animation.isEmpty()){
+            String css = "<style>@keyframes mymove {from {top: 0px;}to {top: 200px;}}</style>";
+            html = html.replace("</style>", css + "</style>");
+            html = html.replace("<h2>Order</h2>", "<h2 style=\"animation:" + animation + "\">Order</h2>");
+        }
+        return html;
     }
 }
--- src/framework/SimpleHtmlFormatter.java
+++ src/framework/SimpleHtmlFormatter.java
@@ -1,11 +1,21 @@
 package framework;
 
 public class SimpleHtmlFormatter extends HtmlFormatter{
+    //使用响应式设计，responsive为true时，添加css样式
+    private boolean responsive;
+
     public SimpleHtmlFormatter(){
         super();
+        this.responsive = false;
     }
 
+    @Override
     public String formatSales(Sales sales){
-        return super.formatSales(sales);
+        String html = super.formatSales(sales);
+        if(responsive){
+            String css = "<style>@media screen and (max-width: 600px) {body { font-size: 18px; }}</style>";
+            html = html.replace("</style>", css + "</style>");
+        }
+        return html;
     }
 }
